191dbab949b95eac0e40bf259c7cb6227f5de843
Yigong Liu <peerdevicenet@gmail.com>
2015-01-06 07:36:11 -0800

    update api

--- src/com/xconns/peerdevicenet/samples/connector_wifi_intent/ConnectorByWifiIntent.java
+++ src/com/xconns/peerdevicenet/samples/connector_wifi_intent/ConnectorByWifiIntent.java
@@ -66,8 +66,8 @@ public class ConnectorByWifiIntent extends Activity {
 					configWifi();
 				} else {
 					Log.d(TAG, "start peer search");
-					Intent in = new Intent(Router.ACTION_START_SEARCH);
-					in.putExtra(Router.SEARCH_TIMEOUT, searchTimeout);
+					Intent in = new Intent(Router.Intent.ACTION_START_SEARCH);
+					in.putExtra(Router.MsgKey.SEARCH_TIMEOUT, searchTimeout);
 					startService(in);
 				}
 			}
@@ -76,7 +76,7 @@ public class ConnectorByWifiIntent extends Activity {
 		mDoneButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				// shutdon router by calling stopService
-				Intent intent = new Intent(Router.ACTION_ROUTER_SHUTDOWN);
+				Intent intent = new Intent(Router.Intent.ACTION_ROUTER_SHUTDOWN);
 				stopService(intent);
 				//
 				finish();
@@ -93,27 +93,27 @@ public class ConnectorByWifiIntent extends Activity {
 		// so it can keep running at background
 		// even when client unbind
 		// must be stopped by calling stopService after all clients unbind
-		Intent intent = new Intent(Router.ACTION_CONNECTION_SERVICE);
+		Intent intent = new Intent(Router.Intent.ACTION_CONNECTION_SERVICE);
 		startService(intent);
 
 		// register recvers to handle ConnectionService related broadcast
 		// intents
 		IntentFilter filter = new IntentFilter();
-		filter.addAction(Router.ACTION_GET_NETWORKS);
-		filter.addAction(Router.ACTION_GET_ACTIVE_NETWORK);
-		filter.addAction(Router.ACTION_ACTIVATE_NETWORK);
-		filter.addAction(Router.ACTION_NETWORK_CONNECTED);
-		filter.addAction(Router.ACTION_NETWORK_DISCONNECTED);
-		filter.addAction(Router.ACTION_SEARCH_START);
-		filter.addAction(Router.ACTION_SEARCH_FOUND_DEVICE);
-		filter.addAction(Router.ACTION_SEARCH_COMPLETE);
-		filter.addAction(Router.ACTION_CONNECTING);
-		filter.addAction(Router.ACTION_CONNECTION_FAILED);
-		filter.addAction(Router.ACTION_CONNECTED);
-		filter.addAction(Router.ACTION_DISCONNECTED);
-		filter.addAction(Router.ACTION_SET_CONNECTION_INFO);
-		filter.addAction(Router.ACTION_GET_CONNECTION_INFO);
-		filter.addAction(Router.ACTION_GET_DEVICE_INFO);
+		filter.addAction(Router.Intent.ACTION_GET_NETWORKS);
+		filter.addAction(Router.Intent.ACTION_GET_ACTIVE_NETWORK);
+		filter.addAction(Router.Intent.ACTION_ACTIVATE_NETWORK);
+		filter.addAction(Router.Intent.ACTION_NETWORK_CONNECTED);
+		filter.addAction(Router.Intent.ACTION_NETWORK_DISCONNECTED);
+		filter.addAction(Router.Intent.ACTION_SEARCH_START);
+		filter.addAction(Router.Intent.ACTION_SEARCH_FOUND_DEVICE);
+		filter.addAction(Router.Intent.ACTION_SEARCH_COMPLETE);
+		filter.addAction(Router.Intent.ACTION_CONNECTING);
+		filter.addAction(Router.Intent.ACTION_CONNECTION_FAILED);
+		filter.addAction(Router.Intent.ACTION_CONNECTED);
+		filter.addAction(Router.Intent.ACTION_DISCONNECTED);
+		filter.addAction(Router.Intent.ACTION_SET_CONNECTION_INFO);
+		filter.addAction(Router.Intent.ACTION_GET_CONNECTION_INFO);
+		filter.addAction(Router.Intent.ACTION_GET_DEVICE_INFO);
 		registerReceiver(mReceiver, filter);
 
 		// setup my device name known to peers
@@ -121,13 +121,13 @@ public class ConnectorByWifiIntent extends Activity {
 		if (myDeviceName == null || myDeviceName.length() == 0) {
 			myDeviceName = "MyDeviceName";
 		}
-		Intent in = new Intent(Router.ACTION_SET_CONNECTION_INFO);
-		in.putExtra(Router.DEVICE_NAME, myDeviceName);
-		in.putExtra(Router.USE_SSL, false);
+		Intent in = new Intent(Router.Intent.ACTION_SET_CONNECTION_INFO);
+		in.putExtra(Router.MsgKey.DEVICE_NAME, myDeviceName);
+		in.putExtra(Router.MsgKey.USE_SSL, false);
 		startService(in);
 
 		// start by checking if device is connected to any networks
-		in = new Intent(Router.ACTION_GET_NETWORKS);
+		in = new Intent(Router.Intent.ACTION_GET_NETWORKS);
 		startService(in);
 
 	}
@@ -193,12 +193,12 @@ public class ConnectorByWifiIntent extends Activity {
 			String pport = null;
 
 			final String action = intent.getAction();
-			if (Router.ACTION_SEARCH_FOUND_DEVICE.equals(action)) {
+			if (Router.Intent.ACTION_SEARCH_FOUND_DEVICE.equals(action)) {
 				// handle msgs for scan
-				pname = intent.getStringExtra(Router.PEER_NAME);
-				paddr = intent.getStringExtra(Router.PEER_ADDR);
-				pport = intent.getStringExtra(Router.PEER_PORT);
-				boolean uSSL = intent.getBooleanExtra(Router.USE_SSL, false);
+				pname = intent.getStringExtra(Router.MsgKey.PEER_NAME);
+				paddr = intent.getStringExtra(Router.MsgKey.PEER_ADDR);
+				pport = intent.getStringExtra(Router.MsgKey.PEER_PORT);
+				boolean uSSL = intent.getBooleanExtra(Router.MsgKey.USE_SSL, false);
 				device = new DeviceInfo(pname, paddr, pport);
 				Log.d(TAG, "onSearchFoundDevice: " + device);
 
@@ -208,63 +208,63 @@ public class ConnectorByWifiIntent extends Activity {
 				if (!connPeers.contains(device.addr)) {
 					if (mDevice.addr.compareTo(device.addr) < 0) {
 						Log.d(TAG, "connect to client: " + device.addr);
-						Intent in = new Intent(Router.ACTION_CONNECT);
-						in.putExtra(Router.PEER_NAME, device.name);
-						in.putExtra(Router.PEER_ADDR, device.addr);
-						in.putExtra(Router.PEER_PORT, device.port);
-						in.putExtra(Router.AUTHENTICATION_TOKEN,
+						Intent in = new Intent(Router.Intent.ACTION_CONNECT);
+						in.putExtra(Router.MsgKey.PEER_NAME, device.name);
+						in.putExtra(Router.MsgKey.PEER_ADDR, device.addr);
+						in.putExtra(Router.MsgKey.PEER_PORT, device.port);
+						in.putExtra(Router.MsgKey.AUTHENTICATION_TOKEN,
 								securityToken.getBytes());
-						in.putExtra(Router.CONNECT_TIMEOUT, connTimeout);
+						in.putExtra(Router.MsgKey.CONNECT_TIMEOUT, connTimeout);
 						startService(in);
 						connPeers.add(device.addr);
 					}
 				}
 
-			} else if (Router.ACTION_SEARCH_COMPLETE.equals(action)) {
+			} else if (Router.Intent.ACTION_SEARCH_COMPLETE.equals(action)) {
 				Log.d(TAG, "search complete");
 				updateGuiSearchComplete();
-			} else if (Router.ACTION_SEARCH_START.equals(action)) {
+			} else if (Router.Intent.ACTION_SEARCH_START.equals(action)) {
 				updateGuiSearchStart();
-			} else if (Router.ACTION_CONNECTED.equals(action)) {
-				pname = intent.getStringExtra(Router.PEER_NAME);
-				paddr = intent.getStringExtra(Router.PEER_ADDR);
-				pport = intent.getStringExtra(Router.PEER_PORT);
+			} else if (Router.Intent.ACTION_CONNECTED.equals(action)) {
+				pname = intent.getStringExtra(Router.MsgKey.PEER_NAME);
+				paddr = intent.getStringExtra(Router.MsgKey.PEER_ADDR);
+				pport = intent.getStringExtra(Router.MsgKey.PEER_PORT);
 				device = new DeviceInfo(pname, paddr, pport);
 				addDeviceToList(device);
 				Log.d(TAG, "a device connected");
-			} else if (Router.ACTION_DISCONNECTED.equals(action)) {
-				pname = intent.getStringExtra(Router.PEER_NAME);
-				paddr = intent.getStringExtra(Router.PEER_ADDR);
-				pport = intent.getStringExtra(Router.PEER_PORT);
+			} else if (Router.Intent.ACTION_DISCONNECTED.equals(action)) {
+				pname = intent.getStringExtra(Router.MsgKey.PEER_NAME);
+				paddr = intent.getStringExtra(Router.MsgKey.PEER_ADDR);
+				pport = intent.getStringExtra(Router.MsgKey.PEER_PORT);
 				connPeers.remove(paddr);
 				device = new DeviceInfo(pname, paddr, pport);
 				delDeviceFromList(device);
 				Log.d(TAG, "a device disconnected: " + device.addr);
-			} else if (Router.ACTION_GET_CONNECTED_PEERS.equals(action)) {
-				String[] names = intent.getStringArrayExtra(Router.PEER_NAMES);
-				String[] addrs = intent.getStringArrayExtra(Router.PEER_ADDRS);
-				String[] ports = intent.getStringArrayExtra(Router.PEER_PORTS);
+			} else if (Router.Intent.ACTION_GET_CONNECTED_PEERS.equals(action)) {
+				String[] names = intent.getStringArrayExtra(Router.MsgKey.PEER_NAMES);
+				String[] addrs = intent.getStringArrayExtra(Router.MsgKey.PEER_ADDRS);
+				String[] ports = intent.getStringArrayExtra(Router.MsgKey.PEER_PORTS);
 				for (int i = 0; i < names.length; i++) {
 					addDeviceToList(new DeviceInfo(names[i], addrs[i], ports[1]));
 				}
 				Log.d(TAG, "get_connected_peers");
-			} else if (Router.ACTION_CONNECTING.equals(action)) {
-				pname = intent.getStringExtra(Router.PEER_NAME);
-				paddr = intent.getStringExtra(Router.PEER_ADDR);
-				pport = intent.getStringExtra(Router.PEER_PORT);
+			} else if (Router.Intent.ACTION_CONNECTING.equals(action)) {
+				pname = intent.getStringExtra(Router.MsgKey.PEER_NAME);
+				paddr = intent.getStringExtra(Router.MsgKey.PEER_ADDR);
+				pport = intent.getStringExtra(Router.MsgKey.PEER_PORT);
 				device = new DeviceInfo(pname, paddr, pport);
 				byte[] token = intent
-						.getByteArrayExtra(Router.AUTHENTICATION_TOKEN);
+						.getByteArrayExtra(Router.MsgKey.AUTHENTICATION_TOKEN);
 				Log.d(TAG, "peer " + device.addr + " sends connecting to me");
 
 				// check if trying to conn to self
 				if (device.addr != null && device.addr.equals(mDevice.addr)) {
 					Log.d(TAG, "CONN_TO_SELF: deny self connection");
-					Intent in = new Intent(Router.ACTION_DENY_CONNECTION);
-					in.putExtra(Router.PEER_NAME, device.name);
-					in.putExtra(Router.PEER_ADDR, device.addr);
-					in.putExtra(Router.PEER_PORT, device.port);
-					in.putExtra(Router.CONN_DENY_CODE,
+					Intent in = new Intent(Router.Intent.ACTION_DENY_CONNECTION);
+					in.putExtra(Router.MsgKey.PEER_NAME, device.name);
+					in.putExtra(Router.MsgKey.PEER_ADDR, device.addr);
+					in.putExtra(Router.MsgKey.PEER_PORT, device.port);
+					in.putExtra(Router.MsgKey.CONN_DENY_CODE,
 							Router.ConnFailureCode.FAIL_CONN_SELF);
 					startService(in);
 					return;
@@ -273,41 +273,41 @@ public class ConnectorByWifiIntent extends Activity {
 				// auto accept connection from peer
 				Log.d(TAG, "accept peer's connection attempt from: "
 						+ device.addr);
-				Intent in = new Intent(Router.ACTION_ACCEPT_CONNECTION);
-				in.putExtra(Router.PEER_NAME, device.name);
-				in.putExtra(Router.PEER_ADDR, device.addr);
-				in.putExtra(Router.PEER_PORT, device.port);
+				Intent in = new Intent(Router.Intent.ACTION_ACCEPT_CONNECTION);
+				in.putExtra(Router.MsgKey.PEER_NAME, device.name);
+				in.putExtra(Router.MsgKey.PEER_ADDR, device.addr);
+				in.putExtra(Router.MsgKey.PEER_PORT, device.port);
 				startService(in);
-			} else if (Router.ACTION_CONNECTION_FAILED.equals(action)) {
-				pname = intent.getStringExtra(Router.PEER_NAME);
-				paddr = intent.getStringExtra(Router.PEER_ADDR);
-				pport = intent.getStringExtra(Router.PEER_PORT);
+			} else if (Router.Intent.ACTION_CONNECTION_FAILED.equals(action)) {
+				pname = intent.getStringExtra(Router.MsgKey.PEER_NAME);
+				paddr = intent.getStringExtra(Router.MsgKey.PEER_ADDR);
+				pport = intent.getStringExtra(Router.MsgKey.PEER_PORT);
 				connPeers.remove(paddr);
 				Log.d(TAG, "connection_failed");
-			} else if (Router.ACTION_GET_DEVICE_INFO.equals(action)) {
-				pname = intent.getStringExtra(Router.PEER_NAME);
-				paddr = intent.getStringExtra(Router.PEER_ADDR);
-				pport = intent.getStringExtra(Router.PEER_PORT);
+			} else if (Router.Intent.ACTION_GET_DEVICE_INFO.equals(action)) {
+				pname = intent.getStringExtra(Router.MsgKey.PEER_NAME);
+				paddr = intent.getStringExtra(Router.MsgKey.PEER_ADDR);
+				pport = intent.getStringExtra(Router.MsgKey.PEER_PORT);
 				device = new DeviceInfo(pname, paddr, pport);
 				mDevice = device;
 				Log.d(TAG, "onGetDeviceInfo: " + device.toString());
 				// my device connect to net and got deviceinfo,
 				// start search for peers
 				Log.d(TAG, "start peer search");
-				Intent in = new Intent(Router.ACTION_START_SEARCH);
-				in.putExtra(Router.SEARCH_TIMEOUT, searchTimeout);
+				Intent in = new Intent(Router.Intent.ACTION_START_SEARCH);
+				in.putExtra(Router.MsgKey.SEARCH_TIMEOUT, searchTimeout);
 				startService(in);
-			} else if (Router.ACTION_ERROR.equals(action)) {
-				String errInfo = intent.getStringExtra(Router.MSG_DATA);
+			} else if (Router.Intent.ACTION_ERROR.equals(action)) {
+				String errInfo = intent.getStringExtra(Router.MsgKey.MSG_DATA);
 				Log.d(TAG, "Error msg: " + errInfo);
-			} else if (Router.ACTION_GET_NETWORKS.equals(action)) {
-				int[] types = intent.getIntArrayExtra(Router.NET_TYPES);
-				String[] names = intent.getStringArrayExtra(Router.NET_NAMES);
-				String[] passes = intent.getStringArrayExtra(Router.NET_PASSES);
-				String[] infos = intent.getStringArrayExtra(Router.NET_INFOS);
+			} else if (Router.Intent.ACTION_GET_NETWORKS.equals(action)) {
+				int[] types = intent.getIntArrayExtra(Router.MsgKey.NET_TYPES);
+				String[] names = intent.getStringArrayExtra(Router.MsgKey.NET_NAMES);
+				String[] passes = intent.getStringArrayExtra(Router.MsgKey.NET_PASSES);
+				String[] infos = intent.getStringArrayExtra(Router.MsgKey.NET_INFOS);
 				String[] intfNames = intent
-						.getStringArrayExtra(Router.NET_INTF_NAMES);
-				String[] addrs = intent.getStringArrayExtra(Router.NET_ADDRS);
+						.getStringArrayExtra(Router.MsgKey.NET_INTF_NAMES);
+				String[] addrs = intent.getStringArrayExtra(Router.MsgKey.NET_ADDRS);
 
 				Log.d(TAG, "onGetNetworks: "
 						+ (names != null ? names.length : "null"));
@@ -327,16 +327,16 @@ public class ConnectorByWifiIntent extends Activity {
 					net0.addr = addrs[0];
 					mNet = net0; // by default activate the first network
 					// first search for current active network
-					Intent in = new Intent(Router.ACTION_GET_ACTIVE_NETWORK);
+					Intent in = new Intent(Router.Intent.ACTION_GET_ACTIVE_NETWORK);
 					startService(in);
 				}
-			} else if (Router.ACTION_GET_ACTIVE_NETWORK.equals(action)) {
-				int type = intent.getIntExtra(Router.NET_TYPE, 0);
-				String name = intent.getStringExtra(Router.NET_NAME);
-				String pass = intent.getStringExtra(Router.NET_PASS);
-				String info = intent.getStringExtra(Router.NET_INFO);
-				String intfName = intent.getStringExtra(Router.NET_INTF_NAME);
-				String addr = intent.getStringExtra(Router.NET_ADDR);
+			} else if (Router.Intent.ACTION_GET_ACTIVE_NETWORK.equals(action)) {
+				int type = intent.getIntExtra(Router.MsgKey.NET_TYPE, 0);
+				String name = intent.getStringExtra(Router.MsgKey.NET_NAME);
+				String pass = intent.getStringExtra(Router.MsgKey.NET_PASS);
+				String info = intent.getStringExtra(Router.MsgKey.NET_INFO);
+				String intfName = intent.getStringExtra(Router.MsgKey.NET_INTF_NAME);
+				String addr = intent.getStringExtra(Router.MsgKey.NET_ADDR);
 				if (name != null) {
 					net = new NetInfo();
 					net.type = type;
@@ -353,31 +353,31 @@ public class ConnectorByWifiIntent extends Activity {
 					// update GUI
 					updateGuiOnNet(net);
 					// get my device info at active network
-					Intent in = new Intent(Router.ACTION_GET_DEVICE_INFO);
+					Intent in = new Intent(Router.Intent.ACTION_GET_DEVICE_INFO);
 					startService(in);
 				} else {// no active network
 					if (mNet != null) {
-						Intent in = new Intent(Router.ACTION_ACTIVATE_NETWORK);
-						in.putExtra(Router.NET_TYPE, mNet.type);
-						in.putExtra(Router.NET_NAME, mNet.name);
-						in.putExtra(Router.NET_PASS, mNet.pass);
-						in.putExtra(Router.NET_INFO, mNet.info);
-						in.putExtra(Router.NET_INTF_NAME, mNet.intfName);
-						in.putExtra(Router.NET_ADDR, mNet.addr);
+						Intent in = new Intent(Router.Intent.ACTION_ACTIVATE_NETWORK);
+						in.putExtra(Router.MsgKey.NET_TYPE, mNet.type);
+						in.putExtra(Router.MsgKey.NET_NAME, mNet.name);
+						in.putExtra(Router.MsgKey.NET_PASS, mNet.pass);
+						in.putExtra(Router.MsgKey.NET_INFO, mNet.info);
+						in.putExtra(Router.MsgKey.NET_INTF_NAME, mNet.intfName);
+						in.putExtra(Router.MsgKey.NET_ADDR, mNet.addr);
 						startService(in);
 					} else {
 						Log.e(TAG, "mNet is null");
 					}
 				}
-			} else if (Router.ACTION_ACTIVATE_NETWORK.equals(action)) {
+			} else if (Router.Intent.ACTION_ACTIVATE_NETWORK.equals(action)) {
 				net = new NetInfo();
-				net.type = intent.getIntExtra(Router.NET_TYPE, 0);
-				net.name = intent.getStringExtra(Router.NET_NAME);
-				net.pass = intent.getStringExtra(Router.NET_PASS);
-				String netinfo = intent.getStringExtra(Router.NET_INFO);
+				net.type = intent.getIntExtra(Router.MsgKey.NET_TYPE, 0);
+				net.name = intent.getStringExtra(Router.MsgKey.NET_NAME);
+				net.pass = intent.getStringExtra(Router.MsgKey.NET_PASS);
+				String netinfo = intent.getStringExtra(Router.MsgKey.NET_INFO);
 				net.info = (netinfo == null) ? null : netinfo.getBytes();
-				net.intfName = intent.getStringExtra(Router.NET_INTF_NAME);
-				net.addr = intent.getStringExtra(Router.NET_ADDR);
+				net.intfName = intent.getStringExtra(Router.MsgKey.NET_INTF_NAME);
+				net.addr = intent.getStringExtra(Router.MsgKey.NET_ADDR);
 				Log.d(TAG, "onNetworkActivated: " + net.toString());
 				mNet = net;
 				// update GUI
@@ -385,36 +385,36 @@ public class ConnectorByWifiIntent extends Activity {
 				// a new network activate, clear foundDevices
 				connPeers.clear();
 				// get my device info at active network
-				Intent in = new Intent(Router.ACTION_GET_DEVICE_INFO);
+				Intent in = new Intent(Router.Intent.ACTION_GET_DEVICE_INFO);
 				startService(in);
-			} else if (Router.ACTION_NETWORK_CONNECTED.equals(action)) {
+			} else if (Router.Intent.ACTION_NETWORK_CONNECTED.equals(action)) {
 				net = new NetInfo();
-				net.type = intent.getIntExtra(Router.NET_TYPE, 0);
-				net.name = intent.getStringExtra(Router.NET_NAME);
-				net.pass = intent.getStringExtra(Router.NET_PASS);
-				String netinfo = intent.getStringExtra(Router.NET_INFO);
+				net.type = intent.getIntExtra(Router.MsgKey.NET_TYPE, 0);
+				net.name = intent.getStringExtra(Router.MsgKey.NET_NAME);
+				net.pass = intent.getStringExtra(Router.MsgKey.NET_PASS);
+				String netinfo = intent.getStringExtra(Router.MsgKey.NET_INFO);
 				net.info = (netinfo == null) ? null : netinfo.getBytes();
-				net.intfName = intent.getStringExtra(Router.NET_INTF_NAME);
-				net.addr = intent.getStringExtra(Router.NET_ADDR);
+				net.intfName = intent.getStringExtra(Router.MsgKey.NET_INTF_NAME);
+				net.addr = intent.getStringExtra(Router.MsgKey.NET_ADDR);
 				Log.d(TAG, "onNetworkConnected: "/* +net.toString() */);
 				// by default activate newly connected network
 				mNet = net;
-				Intent in = new Intent(Router.ACTION_ACTIVATE_NETWORK);
-				in.putExtra(Router.NET_TYPE, mNet.type);
-				in.putExtra(Router.NET_NAME, mNet.name);
-				in.putExtra(Router.NET_PASS, mNet.pass);
-				in.putExtra(Router.NET_INFO, mNet.info);
-				in.putExtra(Router.NET_INTF_NAME, mNet.intfName);
-				in.putExtra(Router.NET_ADDR, mNet.addr);
+				Intent in = new Intent(Router.Intent.ACTION_ACTIVATE_NETWORK);
+				in.putExtra(Router.MsgKey.NET_TYPE, mNet.type);
+				in.putExtra(Router.MsgKey.NET_NAME, mNet.name);
+				in.putExtra(Router.MsgKey.NET_PASS, mNet.pass);
+				in.putExtra(Router.MsgKey.NET_INFO, mNet.info);
+				in.putExtra(Router.MsgKey.NET_INTF_NAME, mNet.intfName);
+				in.putExtra(Router.MsgKey.NET_ADDR, mNet.addr);
 				startService(in);
 
-			} else if (Router.ACTION_NETWORK_DISCONNECTED.equals(action)) {
+			} else if (Router.Intent.ACTION_NETWORK_DISCONNECTED.equals(action)) {
 				Log.d(TAG, "onNetworkDisconnected");
 				mNet = null;
 				updateGuiNoNet();
-			} else if (Router.ACTION_SET_CONNECTION_INFO.equals(action)) {
+			} else if (Router.Intent.ACTION_SET_CONNECTION_INFO.equals(action)) {
 				Log.d(TAG, "finish SetConnectionInfo()");
-			} else if (Router.ACTION_GET_CONNECTION_INFO.equals(action)) {
+			} else if (Router.Intent.ACTION_GET_CONNECTION_INFO.equals(action)) {
 				Log.d(TAG, "onGetConnectionInfo()");
 			} else {
 				Log.d(TAG, "unhandled intent: " + action);
